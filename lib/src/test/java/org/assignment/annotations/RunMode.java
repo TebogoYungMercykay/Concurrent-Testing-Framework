e17a8fe7901c2ae6b12d6f7be6cab10031a032fa
tebogoyungmercykay <u20748052@tuks.co.za>
2024-10-21 00:09:17 +0200

    (test) RunMode Annotation

---
+++ lib/src/test/java/org/assignment/annotations/RunMode.java
@@ -0,0 +1,12 @@
+package org.assignment.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface RunMode {
+    ExecutionMode value() default ExecutionMode.SEQUENTIAL;
+}
